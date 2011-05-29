c941bdfbf83d1969929726fcc1c63c1c1a26eee6
Luigi Scagnet <luigi.scagnet@gmail.com>
2011-05-29 18:15:08 +0200

    Per oggi basta

--- Finestra.py
+++ Finestra.py
@@ -16,11 +16,12 @@ class Finestra:
     __Ip='192.168.1.35'
     __Porta=20000
     __version= '0.2b'
+    __s0=""
     
     def __init__(self):
         self.creaFinestra()
         self.connetti()
-        self.primaLetturaStato()
+        self.setSlider(self.primaLetturaStato())
         self.disconnetti()
         self.__f.mainloop()
                         
@@ -46,7 +47,7 @@ class Finestra:
         self.frameR2C0.configure( width="100", height="150")
         self.frameR2C1.configure( width="100", height="150")
         self.frameR2C2.configure( width="100", height="150")        
-        self.frameR3.configure( width="300", height="25")
+        self.frameR3.configure( width="200", height="30", bd=1, relief=GROOVE)
         
         self.frameR0.grid(row=0, column=0, columnspan=3)
         self.frameR1C0.grid(row=1, column=0)
@@ -67,15 +68,19 @@ class Finestra:
         self.__f.config(menu=menubar)
           
         self.__StatusBar = Label(self.frameR3, text="Connessione in corso..")
-        self.__StatusBar.pack()
+        self.__StatusBar.pack()                        
         
-        self.__ButtonAccendi = Button(self.frameR1C0, text="Accendi")
-        self.__ButtonAccendi.pack({"side":"top", "padx":1, "pady":0})
-        self.__ButtonAccendi['command'] = self.ButtonClickAccendi
+        self.__ButtonAction = Button(self.frameR1C0, text="Accendi" )
+        self.__ButtonAction.pack({"side":"top", "padx":1, "pady":0})
+        self.__ButtonAction['command'] = self.ButtonClickAction
         
-        self.__ButtonSpegni = Button(self.frameR1C2, text="Spegni")
-        self.__ButtonSpegni.pack({"side":"top", "padx":2, "pady":20})
-        self.__ButtonSpegni['command'] = self.ButtonClickSpegni
+        #self.__ButtonAccendi = Button(self.frameR1C0, text="Accendi" )
+        #self.__ButtonAccendi.pack({"side":"top", "padx":1, "pady":0})
+        #self.__ButtonAccendi['command'] = self.ButtonClickAccendi
+        
+        #self.__ButtonSpegni = Button(self.frameR1C2, text="Spegni")
+        #self.__ButtonSpegni.pack({"side":"top", "padx":2, "pady":20})
+        #self.__ButtonSpegni['command'] = self.ButtonClickSpegni
         
         self.__ButtonPiu = Button(self.frameR2C2, text="+")
         self.__ButtonPiu.pack({"side":"top", "padx":2, "pady":2})
@@ -98,7 +103,7 @@ class Finestra:
                 print "Non connesso 0"
                 self.__StatusBar["text"] = "Non connesso"
             else:
-                self.__StatusBar["text"] = "Connesso"
+                self.__StatusBar["text"] = "Connesso a " + self.__Ip
         except socket.error, msg:        
             print "Non connesso 1"
             self.__StatusBar["text"] = "Non connesso"
@@ -114,12 +119,23 @@ class Finestra:
             print "status 1:" + status
             val = status[3:5]
             if val[1] == "*":
-                val = val[0]            
-            print "test: " + val
-            self.__Slider.set(val)
+                val = val[0]                
+            print "Val " + val
+            self.__active = TRUE
+            
+            if val == 0:
+                print "spento"
+                self.__ButtonAction["text"] = "Accendi"
+            else:
+                print "Acceso"
+                self.__ButtonAction["text"] = "Spegni"
+            print "esco con val: " + val
+            return val
+            
         except:
             print "Non connesso 3"
-            self.__StatusBar["text"] = "Non connesso"          
+            self.__StatusBar["text"] = "Non connesso"
+            return 0       
     
     def leggiStato(self):
         try:
@@ -131,20 +147,42 @@ class Finestra:
             print "status 2:" + status
             
             val = status[3:5]
-            if val[1] == "*":
-                val = val[0]            
-            print "test: " + val
-            self.__Slider.set(val)
+            if val[1] == "*" or val[1] == "#":
+                val = val[0] 
+            print "esco con val2: " + val    
+            return val    
+            
+            #self.__Slider.set(val)
                        
         except:
             print "Non connesso 4"
-            self.__StatusBar["text"] = "Non connesso"               
-                              
+            self.__StatusBar["text"] = "Non connesso"         
+            return 0 
+             
+    def setSlider(self, val):
+        print "Pippo: " + val
+        self.__Slider.set(val)  
+    
+                     
     def SliderChange(self, x):
         if self.__active == TRUE:
             self.connetti()
             self.__s.send("*1*"+ x +"*"+ self.__APl +"##")
             self.disconnetti()
+            
+    def ButtonClickAction(self):
+        self.connetti()
+        val = self.leggiStato()
+        if val != 0:
+            print "action: " + val
+            self.__s.send("*1*0*"+ self.__APl +"##")
+        else:
+            print "action: " + val
+            self.__s.send("*1*1*"+ self.__APl +"##")
+        
+        self.leggiStato()
+        self.disconnetti()
+        
             
     def ButtonClickAccendi(self):
         self.connetti()
@@ -161,13 +199,13 @@ class Finestra:
     def ButtonClickPiu(self):
         self.connetti()
         self.__s.send("*1*30*"+ self.__APl +"##")
-        self.leggiStato()
+        self.setSlider(self.leggiStato())
         self.disconnetti()
         
     def ButtonClickMeno(self):
         self.connetti()
         self.__s.send("*1*31*"+ self.__APl +"##")
-        self.leggiStato()
+        self.setSlider(self.leggiStato())
         self.disconnetti()
         
     def disconnetti(self):
