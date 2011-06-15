4d7ab5383add22a8e0abca19a7a128515c5fb0a3
Luca Dariz <luca.dariz@gmail.com>
2011-06-15 18:26:32 +0200

    inizio scheletro dei metodi di gestione rete

--- rete.py
+++ rete.py
@@ -1,3 +1,41 @@
+
+import socket
+
 class Rete:
-    def __init__(self):
+    def __init__(self, io):
+        self.io = io
+
+    """
+    metodi di interfacciamento con l'interfaccia grafica
+    """
+
+    def leggi_stato(self):
+        return 0
+
+    def aumenta_luce(self):
         pass
+
+    def riduci_luce(self):
+        pass
+
+    def accendi(self):
+        pass
+
+    def spegni(self):
+        pass
+
+    """
+    """
+
+    def _connetti(self):
+        pass
+
+    def _disconnetti(self):
+        pass
+
+    def _invia(self, data):
+        pass
+
+    def _ricevi(self):
+        pass
+
